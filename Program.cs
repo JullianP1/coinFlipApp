8602010187a080491134ea384f5bcf08d5454fcf
JullianP1 <jullianp1@learn.cssd.ab.ca>
2018-09-20 14:30:21 -0600

    created int for coin; and a random number generator the picks one of two numbers; created an if statement

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace coinFlip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int coin;
+            string userGuess;
+            Random rng = new Random();
+
+            //queary the user
+            Console.WriteLine("Guess!");
+            userGuess = Console.ReadLine();
+
+            //generat random number
+            coin = rng.Next(0,2);
+
+            if (coin == 0 && userGuess == "T" || coin == 0 && userGuess == "t") {
+                Console.WriteLine("The coin flip was tails, you win!");
+            } else if (coin == 1 && userGuess == "F" || coin == 1 && userGuess == "F") {
+                Console.WriteLine("The coin flip was heads, you win!");
+            }
         }
     }
 }
